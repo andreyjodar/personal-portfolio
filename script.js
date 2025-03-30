cfe11bf45573562f82ab04ae5896f85e7affc885
andreyjodar <andreyviniciusjodar@gmail.com>
2025-03-30 12:49:07 -0300

    Define changeTheme() and refactor variables.css

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ window.onload = function() {
 function closePopup() {
     document.getElementById("popup").style.display="none";
 }
+
+function changeTheme() {
+    const theme = document.body.getAttribute("data-theme");
+    const newTheme = theme == "dark" ? "light" : "dark";
+    document.body.setAttribute("data-theme", newTheme);
+    const btChangeTheme = document.getElementById("change-theme");
+    btChangeTheme.textContent = theme == "dark" ? "Dark" : "Light";
+}
